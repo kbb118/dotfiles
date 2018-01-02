a7c328c8039c6d04e6128ab1932b308f7c71478e
kbbHNM <kbb@HappyNewMeow>
2018-01-02 23:44:39 +0900

    committed automaticaly

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,2 +1,14 @@
-#!/bin/sh
-ln -sf ~/dotfiles/.Xmodmap ~/.Xmodmap
+#!/bin/bash -eu
+
+for f in .??*
+    do
+    # 無視したいファイルやディレクトリはこんな風に追加してね
+    [[ ${f} = ".git" ]] && continue
+    [[ ${f} = ".gitignore" ]] && continue
+    ln -snfv ${HOME}/dotfiles/${f} ${HOME}/${f}
+done
+echo $(tput setaf 2)Deploy dotfiles complete!. ✔︎$(tput sgr0)
+
+git add .
+git commit -m "committed automaticaly"
+git push origin master
