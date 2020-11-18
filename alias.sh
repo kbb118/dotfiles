c4fb1decb4a042c31221d90790cf226847fd1bb1
kob <kob@wsl>
2020-11-19 01:30:54 +0900

    add some alias

--- alias.sh
+++ alias.sh
@@ -1,3 +1,15 @@
+alias ga="git add"
 alias gs="git status"
 alias gd="git diff"
+alias gl="git log"
+alias gci="git commit"
 alias gco="git checkout"
+alias gbr="git branch"
+alias gpulls="git pull > /dev/null 2>&1"
+
+alias ls='ls -F --color=auto'
+alias ll='ls -l'
+alias la='ls -a'
+alias lt='ll -t'
+alias lla='ls -la'
+alias ltr='ll -tr'
